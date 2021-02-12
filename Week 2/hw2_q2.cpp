f8af6135885266c2cd65ab503a39c8ca34b6a5a8
jollylemur <75688716+jollylemur@users.noreply.github.com>
2021-02-12 07:07:11 -0700

    Week 2 push

---
+++ Week 2/hw2_q2.cpp
@@ -0,0 +1,48 @@
+/*
+ja4284_hw2_q2.cpp
+Written by Jacob Altschul
+*/
+
+#include <iostream>
+using namespace std;
+
+const int QUARTER_VALUE_ROUNDED = 25;
+const int DIME_VALUE_ROUNDED = 10;
+const int NICKEL_VALUE_ROUNDED = 5;
+const int PENNY_VALUE_ROUNDED = 1;
+const float QUARTER_VALUE = 0.25;
+
+int main () {
+    int dollars, cents;
+    int quarterDollar, quarters, afterQuarters, finalQuarters;
+    int dimes, afterDimes;
+    int nickels, afterNickels;
+    int pennies;
+
+    //Get information from user
+    cout<<"Please enter your amount in the format of dollars and cents separated by a space:"<<endl;
+    cin>>dollars>>cents;
+    
+    //Calculate quarters
+    quarterDollar = dollars / QUARTER_VALUE;
+    quarters = (int)(cents / QUARTER_VALUE_ROUNDED);
+    afterQuarters = cents - (quarters * QUARTER_VALUE_ROUNDED);
+    finalQuarters = quarterDollar + quarters;
+
+    //Calculate dimes
+    dimes = (int)(afterQuarters / DIME_VALUE_ROUNDED);
+    afterDimes = afterQuarters - (dimes * DIME_VALUE_ROUNDED);
+
+    //Calculate nickels
+    nickels = (int)(afterDimes / NICKEL_VALUE_ROUNDED);
+    afterNickels = afterDimes - (nickels * NICKEL_VALUE_ROUNDED);
+
+    //Calculate pennies
+    pennies = (int)(afterNickels / PENNY_VALUE_ROUNDED);
+
+    //Display final to user
+    cout<<dollars<<" dollars and "<<cents<<" cents are:"<<endl;
+    cout<<finalQuarters<<" quarters, "<<dimes<<" dimes, "<<nickels<<" nickels and "<<pennies<<" pennies"<<endl;
+
+    return 0;
+}
---
+++ Week 2/hw2_q3.cpp
@@ -0,0 +1,55 @@
+/*
+ja4284_hw2_q3.cpp
+Written by Jacob Altschul
+*/
+
+#include <iostream>
+using namespace std;
+
+const int MINUTES_IN_HOUR = 60;
+const int HOURS_IN_DAY = 24;
+
+int main () {
+    int johnMinutes, johnHours, johnDays;
+    int billMinutes, billHours, billDays;
+    int combinedMinutes, combinedHours, combinedDays;
+    int finalMinutes, finalHours, finalDays;
+    int overageMinutes, overageHours;
+
+    //Get John's information from user
+    cout<<"Please enter the number of days John has worked: ";
+    cin>>johnDays;
+    cout<<"Please enter the number of hours John has worked: ";
+    cin>>johnHours;
+    cout<<"Please enter the number of minutes John has worked: ";
+    cin>>johnMinutes;
+
+    //Get Bill's information from user
+    cout<<"\nPlease enter the number of days Bill has worked: ";
+    cin>>billDays;
+    cout<<"Please enter the number of hours Bill has worked: ";
+    cin>>billHours;
+    cout<<"Please enter the number of minutes Bill has worked: ";
+    cin>>billMinutes;
+
+    //combine Bill and John's data
+    combinedDays = johnDays + billDays;
+    combinedHours = johnHours + billHours;
+    combinedMinutes = johnMinutes + billMinutes;
+
+    //calculate minutes
+    finalMinutes = (int)(combinedMinutes % MINUTES_IN_HOUR);
+    overageMinutes = (int)(combinedMinutes / MINUTES_IN_HOUR);
+
+    //calcuate hours
+    finalHours = overageMinutes + (int)(combinedHours % HOURS_IN_DAY);
+    overageHours = (int)(combinedHours / HOURS_IN_DAY);
+
+    //calcuate days
+    finalDays = overageHours + combinedDays;
+
+    //Display final to user
+    cout<<"The total time both of them worked together is: "<<finalDays<<" days, "<<finalHours<<" hours and "<<finalMinutes<<" minutes."<<endl;
+
+    return 0;
+}
